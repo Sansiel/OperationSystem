14d1a2eb2ec2935817ee7767c00889106d2a38f7
zwaren <zwaren@yandex.ru>
2019-05-09 07:23:41 +0400

    And I know it's my place to go~~~

--- src/laba5/Core.java
+++ src/laba5/Core.java
@@ -11,15 +11,22 @@ public class Core {
         ArrayList<Process> processes = new ArrayList<>();
         int processCount = rnd.nextInt(4) + 3;
         for (int i = 0; i < processCount; i++) {
-            processes.add(new Process(i, rnd.nextInt(20) + 10, rnd));
+            Process p = new Process(i, rnd.nextInt(20) + 10, rnd);
+            processes.add(p);
+            System.out.printf("Process: %d, executing time: %d\n", p.getId(), p.getExecutingTime());
         }
 
+        System.out.println("\nBlockingScheduler:");
         BlockingScheduler bs = new BlockingScheduler(processes);
         bs.schedule();
         int bsTime = bs.getCounter();
 
+        System.out.println("\nNonblockingScheduler:");
         NonblockingScheduler ns = new NonblockingScheduler(processes);
         ns.schedule();
         int nsTime = ns.getCounter();
+
+        System.out.println("\nBlockingScheduler's total time:" + bsTime);
+        System.out.println("NonblockingScheduler's total time:" + nsTime);
     }
 }
